c406232d351c7c8e643a90276a9a380c4089a768
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2023-12-27 14:39:07 +0530

    fix: loader overlapping with header bug fixed

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="fixed top-0 left-0 h-full w-full bg-blue-700/50 text-white flex__center">
+    <div className="fixed left-0 h-full w-full bg-blue-700/50 text-white flex__center">
       Loading........
     </div>
   );
