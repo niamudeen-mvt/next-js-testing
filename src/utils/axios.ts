74a64caacd5240451facd088530e099dd5fa421b
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2023-12-29 09:07:55 +0530

    test: production url for api integration set

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { getFromStorage } from "./helper";
 
-const BASE_URL = "http://localhost:7000/api/v1";
+// const BASE_URL = "http://localhost:7000/api/v1";
+const BASE_URL = "https://next-demo-backend.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
